7454c23873a59c1f5c82556c65f8c6fa442a0b7d
Eswar Krishna Maganti <maganti.ek@gmail.com>
2026-06-02 10:08:42 +0530

    updated the builders with more labels for more readabality

--- sw_operator/builders/deployment.py
+++ sw_operator/builders/deployment.py
@@ -1,12 +1,15 @@
 from sw_operator.clients.kubernetes import client
+from kubernetes.client import V1Deployment, V1OwnerReference
+
 
 # utility function to create the deployment specification
-def build_deployment(name, spec, namespace, owner):
+def build_deployment(name: str, spec: dict, namespace: str, owner: V1OwnerReference) -> V1Deployment:
     return client.V1Deployment(
         metadata = client.V1ObjectMeta(
             name = name,
             namespace = namespace,
             owner_references=[owner],
+
         ),
         spec = client.V1DeploymentSpec(
             replicas = spec.get('replicas', 1),
@@ -16,7 +19,11 @@ def build_deployment(name, spec, namespace, owner):
             template = client.V1PodTemplateSpec(
                 metadata = client.V1ObjectMeta(
                     name=name,
-                    labels={"app": name},
+                    labels={
+                        "app.kubernetes.io/name": name,
+                        "app.kubernetes.io/managed-by": "staticwebsite-operator",
+                        "app.kubernetes.io/component" : "staticwebsite"
+                    },
                 ),
                 spec = client.V1PodSpec(
                     containers = [
--- sw_operator/builders/service.py
+++ sw_operator/builders/service.py
@@ -1,15 +1,24 @@
 from sw_operator.clients.kubernetes import client
+from kubernetes.client import V1Service, V1OwnerReference
 
 # utility function to build the service manifest
-def build_service(name, spec, namespace, owner):
+def build_service(name: str, spec: dict, namespace: str, owner: V1OwnerReference ) -> V1Service:
     return client.V1Service(
         metadata = client.V1ObjectMeta(
             name = name,
             namespace = namespace,
             owner_references = [owner],
+            labels = {
+                "app.kubernetes.io/name" : name,
+                "app.kubernetes.io/managed-by": "staticwebsite-operator",
+                "app.kubernetes.io/component": "staticwebsite"
+            }
         ),
         spec = client.V1ServiceSpec(
-            selector =  {"app": name},
+            selector =  {
+                "app.kubernetes.io/name": name,
+                "app.kubernetes.io/managed-by": "staticwebsite-operator",
+            },
             type = "NodePort",
             ports = [
                 client.V1ServicePort(
