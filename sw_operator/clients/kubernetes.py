300c982ae9a8513e8655df8e3ebd3b6154e80011
Eswar Krishna Maganti <maganti.ek@gmail.com>
2026-06-10 19:31:39 +0530

    Fixing the runtime issue with the container auth with cluster using service account

--- sw_operator/clients/kubernetes.py
+++ sw_operator/clients/kubernetes.py
@@ -1,11 +1,12 @@
 from kubernetes import client, config
+from kubernetes.config import ConfigException
 
 # Load the kubeconfig
 def _load_config():
     try:
         # running inside a cluster (production)
         config.load_incluster_config()
-    except:
+    except ConfigException:
         # Running locally or in CT
         config.load_kube_config()
 
